539dcda905d9e81acacff383b9c946cd889959ab
Matthijs van der Burgh <matthijs.vander.burgh@live.nl>
2016-04-12 13:04:10 +0200

    to test old firmware on extern TUeES030

--- src/TUeES030.cpp
+++ src/TUeES030.cpp
@@ -1,7 +1,5 @@
 /***************************************************************************
- tag:
- V2: Matthijs van der Burgh
- V1: Ton Peters, Max Baeten, Ruud van den Bogaert
+ tag: Ton Peters, Max Baeten, Ruud van den Bogaert
  Driver for TUeES030
 
  ***************************************************************************
@@ -27,58 +25,45 @@
 using namespace soem_beckhoff_drivers;
 
 TUeES030::TUeES030(ec_slavet* mem_loc) :
-	soem_master::SoemDriver(mem_loc), 
+    soem_master::SoemDriver(mem_loc),
             port_out_digitalIns("digitalIns"),
             port_out_encoder1("encoder1"),
             port_out_encoder2("encoder2"),
             port_out_encoder3("encoder3"),
-            port_out_enc_time1("encoder_timestamp1"),
-            port_out_enc_time2("encoder_timestamp2"),
-            port_out_enc_time3("encoder_timestamp3"),
-            port_out_enc_vels("encoder_velocities"),
             port_out_currents("currents"),
             port_out_caliphers("caliphers"),
             port_out_forceSensors("forceSensors"),
             port_out_positionSensors("positionSensors"),
             port_out_analogIns("analogIns"),
-            port_out_linevoltage("linevoltage"),
             port_out_timeStamp("timeStamp"),
             port_in_digitalOuts("digitalOuts"),
             port_in_pwmDutyMotors("pwmDutyMotors"),
             port_in_analogOuts("analogOuts"),
             port_in_enable("enablePort"){
 
-		m_service->doc(
-			std::string("Services for custom EtherCat ") + std::string(
-					m_datap->name) + std::string(" module"));
+        m_service->doc(
+            std::string("Services for custom EtherCat ") + std::string(
+                    m_datap->name) + std::string(" module"));
 
     m_service->addOperation("write_pwm", &TUeES030::write_pwm, this, RTT::OwnThread).doc("Write pwm duty values");
     m_service->addOperation("write_analog_out", &TUeES030::write_analog_out, this, RTT::OwnThread).doc("Write analog out values");
-	m_service->addOperation("read_digital_ins", &TUeES030::read_digital_ins, this, RTT::OwnThread).doc("Read digital inputs");
-	m_service->addOperation("read_encoders", &TUeES030::read_encoders, this, RTT::OwnThread).doc("Read encoder values");
-    m_service->addOperation("read_encoder_times", &TUeES030::read_encoder_times, this, RTT::OwnThread).doc("Read encoder timestamps");
-    m_service->addOperation("read_encoder_vels", &TUeES030::read_encoder_vels, this, RTT::OwnThread).doc("Read encoder velocities");
-	m_service->addOperation("read_currents", &TUeES030::read_currents, this, RTT::OwnThread).doc("Read current values");
+    m_service->addOperation("read_digital_ins", &TUeES030::read_digital_ins, this, RTT::OwnThread).doc("Read digital inputs");
+    m_service->addOperation("read_encoders", &TUeES030::read_encoders, this, RTT::OwnThread).doc("Read encoder values");
+    m_service->addOperation("read_currents", &TUeES030::read_currents, this, RTT::OwnThread).doc("Read current values");
     m_service->addOperation("read_caliphers", &TUeES030::read_caliphers, this, RTT::OwnThread).doc("Read calipher values");
     m_service->addOperation("read_forces", &TUeES030::read_forces, this, RTT::OwnThread).doc("Read force sensor values");
     m_service->addOperation("read_positions", &TUeES030::read_positions, this, RTT::OwnThread).doc("Read position sensor values");
-    m_service->addOperation("read_linevoltage", &TUeES030::read_linevoltage, this, RTT::OwnThread).doc("Read linevoltage");
     m_service->addOperation("read_time_stamp", &TUeES030::read_time_stamp, this, RTT::OwnThread).doc("Read time stamp");
 
-	m_service->addPort(port_out_digitalIns).doc("");
+    m_service->addPort(port_out_digitalIns).doc("");
     m_service->addPort(port_out_encoder1).doc("");
     m_service->addPort(port_out_encoder2).doc("");
     m_service->addPort(port_out_encoder3).doc("");
-    m_service->addPort(port_out_enc_time1).doc("");
-    m_service->addPort(port_out_enc_time2).doc("");
-    m_service->addPort(port_out_enc_time3).doc("");
-    m_service->addPort(port_out_enc_vels).doc("");
     m_service->addPort(port_out_currents).doc("");
     m_service->addPort(port_out_caliphers).doc("");
     m_service->addPort(port_out_forceSensors).doc("");
-	m_service->addPort(port_out_positionSensors).doc("");
+    m_service->addPort(port_out_positionSensors).doc("");
     m_service->addPort(port_out_analogIns).doc("");
-    m_service->addPort(port_out_linevoltage).doc("");
     m_service->addPort(port_out_timeStamp).doc("");
     m_service->addPort(port_in_digitalOuts).doc("");
     m_service->addPort(port_in_pwmDutyMotors).doc("");
@@ -89,33 +74,25 @@ TUeES030::TUeES030(ec_slavet* mem_loc) :
     encoder1_msg.value = 0;
     encoder2_msg.value = 0;
     encoder3_msg.value = 0;
-    enc_time1_msg.value=0;
-    enc_time2_msg.value=0;
-    enc_time3_msg.value=0;
-    enc_vels_msg.values.assign(3, 0.0);
-    currents_msg.values.assign(3, 0.0);
-    caliphers_msg.values.assign(2, 0.0);
+    currents_msg.values.assign(3,0.0);
+    caliphers_msg.values.assign(2,0.0);
     forceSensors_msg.values.assign(3, 0.0);
     positionSensors_msg.values.assign(3, 0.0);
-    analogIns_msg.values.assign(2, 0.0);
-    linevoltage_msg.value=0;
+    analogIns_msg.values.assign(2,0.0);
     timeStamp_msg.value = 0;
-    digitalOuts_msg.values.assign(2, 0.0);
-    analogOuts_msg.values.assign(2, 0.0);
-    pwmDutyMotors_msg.values.assign(3, 0.0);
+    digitalOuts_msg.values.assign(2,0.0);
+    analogOuts_msg.values.assign(2,0.0);
+    pwmDutyMotors_msg.values.assign(3,0.0);
 
-    analogconverter=4095.0*3.3;
 }
 
-
-
 bool TUeES030::configure() {
-	
+
     // initialize variables
     enablestatus = false;
-	enable = false;
-	port_enabled_was_connected = false;
-	
+    enable = false;
+    port_enabled_was_connected = false;
+
     cntr = 1;
     printEnabled = 0;
     printDisabled = 1;
@@ -123,9 +100,9 @@ bool TUeES030::configure() {
     digitalin.port = 0;
     digitalin_prev.port = 0;
     digitalout.port = 0;
-    
-	print_counter = 0;
-	return true;
+
+    print_counter = 0;
+    return true;
 }
 
 bool TUeES030::start() {
@@ -140,7 +117,7 @@ bool TUeES030::start() {
 }
 
 void TUeES030::update() {
-	
+
     // check if port_in_enable is connected and read the value
     if (!port_in_enable.connected() && port_enabled_was_connected) {
         enable = false;
@@ -150,60 +127,57 @@ void TUeES030::update() {
         }
     }
     else if (port_in_enable.connected()) {
-		port_enabled_was_connected = true;
+        port_enabled_was_connected = true;
         port_in_enable.read(enable);
         if (enablestatus == true) {
             log(Warning)<<"TUeES030 Driver: Port_in_enable is connected"<<endlog();
-			enablestatus = false;
+            enablestatus = false;
         }
     }
 
     // get the pointers to the communication structs
     m_in_tueEthercat = ((in_tueEthercatMemoryt*) (m_datap->inputs));
     m_out_tueEthercat = ((out_tueEthercatMemoryt*) (m_datap->outputs));
-	
-	// read the data from the ethercat memory input and send to orocos
-	read_digital_ins();
+
+    // read the data from the ethercat memory input and send to orocos
+    read_digital_ins();
     read_encoders();
-    read_encoder_times();
-    read_encoder_vels();
-    read_linevoltage();
-	read_time_stamp();
+    read_time_stamp();
     read_currents();
     read_caliphers();
     read_forces();
-	read_positions();
+    read_positions();
 
-	// enable = true, all outputs are send to Slave
-	// enable = fales, all outputs set to zero
-	if (enable) {
-		// digital outputs
-		if (port_in_digitalOuts.connected()) {
-			if (port_in_digitalOuts.read(digitalOuts_msg) == NewData) {
-				digitalout.line.enable_1 = digitalOuts_msg.values[0];
-				digitalout.line.enable_2 = digitalOuts_msg.values[1];
-				digitalout.line.spare_do_3 = digitalOuts_msg.values[2];
-				digitalout.line.spare_do_4 = digitalOuts_msg.values[3];
-			}
-		}
-		// pwm duty motors
-		if (port_in_pwmDutyMotors.connected()) {
-			if (port_in_pwmDutyMotors.read(pwmDutyMotors_msg) == NewData) {
-				write_pwm((pwmDutyMotors_msg.values[0]),(pwmDutyMotors_msg.values[1]),(pwmDutyMotors_msg.values[2]));
-			}
-		}
-		// analog outputs
-		if (port_in_analogOuts.connected()) {
-			if (port_in_analogOuts.read(analogOuts_msg) == NewData) {
-				write_analog_out(analogOuts_msg.values[0],analogOuts_msg.values[1]);
-			}
-		}
-	}
-	else {
-		digitalout.port = 0;
-		write_pwm((float)(0.0),(float)(0.0),(float)(0.0));
+    // enable = true, all outputs are send to Slave
+    // enable = fales, all outputs set to zero
+    if (enable) {
+        // digital outputs
+        if (port_in_digitalOuts.connected()) {
+            if (port_in_digitalOuts.read(digitalOuts_msg) == NewData) {
+                digitalout.line.enable_1 = digitalOuts_msg.values[0];
+                digitalout.line.enable_2 = digitalOuts_msg.values[1];
+                digitalout.line.spare_do_3 = digitalOuts_msg.values[2];
+                digitalout.line.spare_do_4 = digitalOuts_msg.values[3];
+            }
+        }
+        // pwm duty motors
+        if (port_in_pwmDutyMotors.connected()) {
+            if (port_in_pwmDutyMotors.read(pwmDutyMotors_msg) == NewData) {
+                write_pwm((pwmDutyMotors_msg.values[0]),(pwmDutyMotors_msg.values[1]),(pwmDutyMotors_msg.values[2]));
+            }
+        }
+        // analog outputs
+        if (port_in_analogOuts.connected()) {
+            if (port_in_analogOuts.read(analogOuts_msg) == NewData) {
+                write_analog_out(analogOuts_msg.values[0],analogOuts_msg.values[1]);
+            }
+        }
+    }
+    else {
+        digitalout.port = 0;
+        write_pwm((float)(0.0),(float)(0.0),(float)(0.0));
         write_analog_out((float)(0.0),(float)(0.0));
-	}
+    }
     // set digital outputs to ethercat memory output
     m_out_tueEthercat->digital_out = digitalout;
 
@@ -211,8 +185,8 @@ void TUeES030::update() {
     if (digitalin.line.power_status != digitalin_prev.line.power_status) {
         if (!digitalin.line.power_status) {
             log(Warning)<< "TUeES030 Driver Status:  Power OK" << endlog();
-		}
-		else {
+        }
+        else {
             log(Warning)<< "TUeES030 Driver Status:  Power down" << endlog();
         }
     }
@@ -221,14 +195,14 @@ void TUeES030::update() {
 
 void TUeES030::read_digital_ins(){
 
-	digitalin = m_in_tueEthercat->digital_in;
+    digitalin = m_in_tueEthercat->digital_in;
 
-	digitalIns_msg.values[0] = digitalin.line.spare_di_1;
-	digitalIns_msg.values[1] = digitalin.line.spare_di_2;
-	digitalIns_msg.values[2] = digitalin.line.spare_di_3;
-	digitalIns_msg.values[3] = digitalin.line.spare_di_4;
+    digitalIns_msg.values[0] = digitalin.line.spare_di_1;
+    digitalIns_msg.values[1] = digitalin.line.spare_di_2;
+    digitalIns_msg.values[2] = digitalin.line.spare_di_3;
+    digitalIns_msg.values[3] = digitalin.line.spare_di_4;
 
-	port_out_digitalIns.write(digitalIns_msg);
+    port_out_digitalIns.write(digitalIns_msg);
 }
 
 void TUeES030::read_encoders(){
@@ -242,34 +216,6 @@ void TUeES030::read_encoders(){
     port_out_encoder1.write(encoder1_msg);
 
     //log(Info) << "Angles: ["<< enc1 << ", "<< enc2 << ", " << enc3 << "]" << endlog();
-}
-
-void TUeES030::read_encoder_times(){
-
-    enc_time1_msg.value = m_in_tueEthercat->timestamp1;
-    enc_time2_msg.value = m_in_tueEthercat->timestamp2;
-    enc_time3_msg.value = m_in_tueEthercat->timestamp3;
-
-    port_out_enc_time3.write(enc_time3_msg);
-    port_out_enc_time2.write(enc_time2_msg);
-    port_out_enc_time1.write(enc_time1_msg);
-
-    //log(Info) << "Timestamps: ["<< enc1 << ", "<< enc2 << ", " << enc3 << "]" << endlog();
-}
-
-void TUeES030::read_encoder_vels(){
-
-    float velocity1 = (float) m_in_tueEthercat->velocity1;
-    float velocity2 = (float) m_in_tueEthercat->velocity2;
-    float velocity3 = (float) m_in_tueEthercat->velocity3;
-
-    // conversion from bit to rad/s
-    enc_vels_msg.values[0] = (velocity1*0.1);
-    enc_vels_msg.values[1] = (velocity2*0.1);
-    enc_vels_msg.values[2] = (velocity3*0.1);
-
-    port_out_enc_vels.write(enc_vels_msg);
-    //log(Info) << "Velocities: ["<< velocity1 << ", "<< velocity2 << ", " << velocity3 << "]" << endlog();
 }
 
 void TUeES030::read_currents(){
@@ -288,31 +234,31 @@ void TUeES030::read_currents(){
 
 void TUeES030::read_caliphers(){
 
-	// Note, the caliphers are connected wrong
-	// calipher 2 is attached to the legs and calipher 1 to the trunk
-	float calipher2 = (float)m_in_tueEthercat->calipher_1;
-	float calipher1 = (float)m_in_tueEthercat->calipher_2;
-	
-	// conversion  1 bit = 0.01mm
+    // Note, the caliphers are connected wrong
+    // calipher 2 is attached to the legs and calipher 1 to the trunk
+    float calipher2 = (float)m_in_tueEthercat->calipher_1;
+    float calipher1 = (float)m_in_tueEthercat->calipher_2;
+
+    // conversion  1 bit = 0.01mm
     caliphers_msg.values[0] = (calipher1*0.00001);
     caliphers_msg.values[1] = (calipher2*0.00001);
-    
+
     port_out_caliphers.write(caliphers_msg);
 }
 
 void TUeES030::read_forces(){
-		
+
     float force1 = (float) m_in_tueEthercat->force_1;
     float force2 = (float) m_in_tueEthercat->force_2;
     float force3 = (float) m_in_tueEthercat->force_3;
-	
-     forceSensors_msg.values[0] = (force1/analogconverter);   	 //12 bits over 3.3V
-     forceSensors_msg.values[1] = (force2/analogconverter);   	 //12 bits over 3.3V
-     forceSensors_msg.values[2] = (force3/analogconverter);   	 //12 bits over 3.3V
 
-	 port_out_forceSensors.write(forceSensors_msg);
+     forceSensors_msg.values[0] = (force1/4095.0*3.3);   	 //12 bits over 3.3V
+     forceSensors_msg.values[1] = (force2/4095.0*3.3);   	 //12 bits over 3.3V
+     forceSensors_msg.values[2] = (force3/4095.0*3.3);   	 //12 bits over 3.3V
 
-	//log(Warning) << "Forces: ["<< force1 << ", "<< force2 << ", " << force3 << "]" << endlog();
+     port_out_forceSensors.write(forceSensors_msg);
+
+    //log(Warning) << "Forces: ["<< force1 << ", "<< force2 << ", " << force3 << "]" << endlog();
 }
 
 void TUeES030::read_positions(){
@@ -320,20 +266,14 @@ void TUeES030::read_positions(){
     float position1 = (float) m_in_tueEthercat->position_1;
     float position2 = (float) m_in_tueEthercat->position_2;
     float position3 = (float) m_in_tueEthercat->position_3;
-    
-    positionSensors_msg.values[0] = (position1/analogconverter);   	 //12 bits over 3.3V
-    positionSensors_msg.values[1] = (position2/analogconverter);   	 //12 bits over 3.3V
-    positionSensors_msg.values[2] = (position3/analogconverter);   	 //12 bits over 3.3V
+
+    positionSensors_msg.values[0] = (position1/4095.0*3.3);   	 //12 bits over 3.3V
+    positionSensors_msg.values[1] = (position2/4095.0*3.3);   	 //12 bits over 3.3V
+    positionSensors_msg.values[2] = (position3/4095.0*3.3);   	 //12 bits over 3.3V
 
     port_out_positionSensors.write(positionSensors_msg);
 
     // log(Info) << "Positions: ["<< position1 << ", "<< position2 << ", " << position3 << "]" << endlog();
-}
-
-void TUeES030::read_linevoltage(){
-
-    linevoltage_msg.value = m_in_tueEthercat->linevoltage;
-    port_out_linevoltage.write(linevoltage_msg);
 }
 
 void TUeES030::read_time_stamp(){
@@ -345,35 +285,34 @@ void TUeES030::read_time_stamp(){
 
 void TUeES030::write_pwm(float val1, float val2, float val3) {
     int16 tmp1 = (int16) val1;
-	int16 tmp2 = (int16) val2;
-	int16 tmp3 = (int16) val3;
+    int16 tmp2 = (int16) val2;
+    int16 tmp3 = (int16) val3;
 
     // saturate the pwm duty cycles
-    // PWM 1: 6A, PWM 2,3: 3A
-    if (tmp1 > 6000)
-        tmp1 = 6000;
-    if (tmp1 < -6000)
-        tmp1 = -6000;
+    if (tmp1 > 1000)
+        tmp1 = 1000;
+    if (tmp1 < -1000)
+        tmp1 = -1000;
 
-    if (tmp2 > 3000)
-        tmp2 = 3000;
-    if (tmp2 < -3000)
-        tmp2 = -3000;
+    if (tmp2 > 1000)
+        tmp2 = 1000;
+    if (tmp2 < -1000)
+        tmp2 = -1000;
 
-    if (tmp3 > 3000)
-        tmp3 = 3000;
-    if (tmp3 < -3000)
-        tmp3 = -3000;
-		
+    if (tmp3 > 1000)
+        tmp3 = 1000;
+    if (tmp3 < -1000)
+        tmp3 = -1000;
+
     m_out_tueEthercat->pwm_duty_motor_1 = tmp1;
     m_out_tueEthercat->pwm_duty_motor_2 = tmp2;
     m_out_tueEthercat->pwm_duty_motor_3 = tmp3;
-    
+
     //if (print_counter>1000) {
-	//	log(Warning) << "output pwms are " << tmp1 <<" and " << tmp2 << endlog();
-	//	print_counter = 0;
-	//}
-	//print_counter++;
+    //	log(Warning) << "output pwms are " << tmp1 <<" and " << tmp2 << endlog();
+    //	print_counter = 0;
+    //}
+    //print_counter++;
 }
 
 void TUeES030::write_analog_out(float val1, float val2) {
@@ -397,7 +336,7 @@ void TUeES030::write_analog_out(float val1, float val2) {
 }
 
 void TUeES030::stop() {
-	return;
+    return;
 }
 
 namespace {
@@ -406,6 +345,6 @@ soem_master::SoemDriver* createTUeES030(ec_slavet* mem_loc) {
 }
 
 const bool registered0 =
-		soem_master::SoemDriverFactory::Instance().registerDriver(
+        soem_master::SoemDriverFactory::Instance().registerDriver(
                 "TUeES030", createTUeES030);
 }
--- src/TUeES030.hpp
+++ src/TUeES030.hpp
@@ -1,7 +1,5 @@
 /***************************************************************************
- tag:
- V2: Matthijs van der Burgh
- V1: Ton Peters, Max Baeten, Ruud van den Bogaert
+ tag: Ton Peters, Max Baeten, Ruud van den Bogaert
  Driver for TUeES030
 
  ***************************************************************************
@@ -45,7 +43,7 @@ typedef vector<double> doubles;
 // WARNING, the bits are numbered in reversed order
 typedef union PACKED {
     struct PACKED {
-        uint8 spare_di_1:1;     // bit 0
+        uint8 spare_di_1:1;		// bit 0
         uint8 spare_di_2:1;
         uint8 spare_di_3:1;
         uint8 spare_di_4:1;
@@ -57,7 +55,6 @@ typedef union PACKED {
     uint8   port;
 } digital_in_t;
 
-/*
 typedef struct PACKED {
         digital_in_t digital_in;        // Digital inputs
         uint16 encoder_1;               // Encoder 1
@@ -68,88 +65,39 @@ typedef struct PACKED {
         uint16 current_3;               // Current 3
         uint16 calipher_1;              // calipher 1 (1 bit = 0.01 mm)
         uint16 calipher_2;              // calipher 2 (1 bit = 0.01 mm)
-        uint16 force_1;                 // Analog ADC value of force sensor input 1
-        uint16 force_2;                 // Analog ADC value of force sensor input 2
-        uint16 force_3;                 // Analog ADC value of force sensor input 3
-        uint16 position_1;              // Analog ADC value of position sensor 1
-        uint16 position_2;              // Analog ADC value of position sensor 2
-        uint16 position_3;              // Analog ADC value of position sensor 3
+        uint16 force_1; 				// Analog ADC value of force sensor input 1
+        uint16 force_2; 				// Analog ADC value of force sensor input 2
+        uint16 force_3; 				// Analog ADC value of force sensor input 3
+        uint16 position_1; 				// Analog ADC value of position sensor 1
+        uint16 position_2; 				// Analog ADC value of position sensor 2
+        uint16 position_3; 				// Analog ADC value of position sensor 3
         uint16 spare_ai_1;              // Spare analog in 1
         uint16 spare_ai_2;              // Spare analog in 2
         uint16 time_stamp;              // Time stamp (1 bit equals 256 ns)
-    } in_tueEthercatMemoryt; */
-    
-typedef struct PACKED
-{ 
-    uint8       mstate1;			// motor state 1
-    uint32      encoder_1;			// Encoder 1
-    uint32      timestamp1;			// Time stamp encoder 1 (Only changes, if encoder changes)
-    int16       velocity1;			// Velocity encoder 1; 1 bit=0.1 rad/s; depending on parameters on slave
-    int16       current_1;			// current on PWM 1 (1 bit = 1 mA)
-    uint8       mstate2;			// motor state 2
-    uint32      encoder_2;			// Encoder 2
-    uint32      timestamp2;			// Time stamp encoder 2 (Only changes, if encoder changes)
-    int16       velocity2;			// Velocity encoder 2; 1 bit=0.1 rad/s; depending on parameters on slave
-    int16       current_2;			// current on PWM 2 (1 bit = 1 mA)
-    uint8       mstate3;            // motor state 3
-    uint32      encoder_3;			// Encoder 3
-    uint32      timestamp3;			// Time stamp encoder 3 (Only changes, if encoder changes)
-    int16       velocity3;			// Velocity encoder 3; 1 bit=0.1 rad/s; depending on parameters on slave
-    int16       current_3;			// current on PWM 2 (1 bit = 1 mA)
-    digital_in_t digital_in;        // digital input 8 bits
-    uint16      calipher_1;			// calipher 1 (1 bit = 0.01 mm)
-    uint16      calipher_2;			// calipher 2 (1 bit = 0.01 mm)
-    uint16      force_1;			// Analog ADC value of force sensor input 1
-    uint16      force_2;			// Analog ADC value of force sensor input 2
-    uint16      force_3;			// Analog ADC value of force sensor input 3
-    uint16      position_1;			// Analog ADC value of position sensor 1
-    uint16      position_2;			// Analog ADC value of position sensor 2
-    uint16      position_3;			// Analog ADC value of position sensor 3
-    uint16      spare_ai_1;			// Spare analog in 1
-    uint16      spare_ai_2;			// Spare analog in 2
-    uint16      linevoltage;        // 2500 bits = 25V; 1 bit = 0,01V
-    uint16      time_stamp;
-} in_tueEthercatMemoryt;    
+    } in_tueEthercatMemoryt;
 
 // WARNING, the bits are numbered in reversed order
 typedef union PACKED {
-    struct PACKED { 
-        uint8 enable_1:1;       // bit 0
-        uint8 enable_2:1;   
+    struct PACKED {
+        uint8 enable_1:1;		// bit 0
+        uint8 enable_2:1;
         uint8 spare_do_3:1;
-        uint8 spare_do_4:1; 
+        uint8 spare_do_4:1;
         uint8 reserved_1:1;
         uint8 reserved_2:1;
         uint8 reserved_3:1;
-        uint8 reserved_4:1;     // bit 7
+        uint8 reserved_4:1;		// bit 7
     }       line;
     uint8   port;
 } digital_out_t;
 
-/*
 typedef struct PACKED {
     digital_out_t digital_out;      // Digital outputs
-    int16 pwm_duty_motor_1;         // PWM duty cycle for motor 1 (limited from -1000 up to 1000, 0 is no motion)
-    int16 pwm_duty_motor_2;         // PWM duty cycle for motor 2
-    int16 pwm_duty_motor_3;         // PWM duty cycle for motor 3
+    int16 pwm_duty_motor_1; 		// PWM duty cycle for motor 1 (limited from -1000 up to 1000, 0 is no motion)
+    int16 pwm_duty_motor_2; 		// PWM duty cycle for motor 2
+    int16 pwm_duty_motor_3; 		// PWM duty cycle for motor 3
     int16 analog_out_1;             // Analog output 1  (0V = -2048, 10V = 2047, 5V = 0 is no motion)
     int16 analog_out_2;             // Analog output 2
-} out_tueEthercatMemoryt;*/
-
-typedef struct PACKED
-{
-    uint8       mcom1;              // motor 1 command (bit 0..1 = motor channel , bit 2 = enable , bit 3 = tristate active) (00=braked, 01=tristate, 10/11=controlled)
-    int16       pwm_duty_motor_1;   // current setpoint 1 (1 bit = 1 mA) 6A continiuous current
-    int16       ff1;                // current feed forward 1 (1 bit = 1 mA)
-    uint8       mcom2;              // motor 2 command (bit 0..1 = motor channel , bit 2 = enable , bit 3 = tristate active) (00=braked, 01=tristate, 10/11=controlled)
-    int16       pwm_duty_motor_2;   // current setpoint 2 (1 bit = 1 mA) 3A continuous current
-    int16       ff2;                // current feed forward 2 (1 bit = 1 mA)
-    uint8       mcom3;              // motor 3 command (bit 0..1 = motor channel , bit 2 = enable , bit 3 = tristate active) (00=braked, 01=tristate, 10/11=controlled)
-    int16       pwm_duty_motor_3;   // current setpoint 3 (1 bit = 1 mA) 3A continuous current
-    int16       ff3;                // current feed forward 3 (1 bit = 1 mA)
-    digital_out_t digital_out;      // digital output 8 bits
-    int16       analog_out_1;       // analog output 1  (0V = -2048, 10V = 2047, 5V = 0 is no motion)
-    int16       analog_out_2;       // analog output 2
 } out_tueEthercatMemoryt;
 
 using namespace RTT;
@@ -166,14 +114,11 @@ namespace soem_beckhoff_drivers {
         bool start();
         void read_digital_ins();
         void read_encoders();
-        void read_encoder_times();
-        void read_encoder_vels();
         void read_currents();
         void read_caliphers();
         void read_forces();
         void read_positions();
         void read_analog_ins();
-        void read_linevoltage();
         void read_time_stamp();
         void write_pwm(float val1,float val2,float val3);
         void write_analog_out(float val1, float val2);
@@ -196,16 +141,11 @@ namespace soem_beckhoff_drivers {
         EncoderMsg encoder1_msg;
         EncoderMsg encoder2_msg;
         EncoderMsg encoder3_msg;
-        EncoderMsg enc_time1_msg;
-        EncoderMsg enc_time2_msg;
-        EncoderMsg enc_time3_msg;
-        AnalogMsg  enc_vels_msg;
         AnalogMsg  currents_msg;
         AnalogMsg  caliphers_msg;
         AnalogMsg  forceSensors_msg;
         AnalogMsg  positionSensors_msg;
         AnalogMsg  analogIns_msg;
-        EncoderMsg linevoltage_msg;
         EncoderMsg timeStamp_msg;
         DigitalMsg digitalOuts_msg;
         AnalogMsg  pwmDutyMotors_msg;
@@ -217,19 +157,14 @@ namespace soem_beckhoff_drivers {
 
         // Declaring of In and Out ports
         OutputPort<DigitalMsg> port_out_digitalIns;
-        OutputPort<EncoderMsg> port_out_encoder1;   // Make a new message type for
-        OutputPort<EncoderMsg> port_out_encoder2;   // multiple encoder values.
+        OutputPort<EncoderMsg> port_out_encoder1; // Make a new message type for
+        OutputPort<EncoderMsg> port_out_encoder2; // multiple encoder values.
         OutputPort<EncoderMsg> port_out_encoder3;
-        OutputPort<EncoderMsg> port_out_enc_time1;
-        OutputPort<EncoderMsg> port_out_enc_time2;
-        OutputPort<EncoderMsg> port_out_enc_time3;
-        OutputPort<AnalogMsg>  port_out_enc_vels;
         OutputPort<AnalogMsg>  port_out_currents;
         OutputPort<AnalogMsg>  port_out_caliphers;
         OutputPort<AnalogMsg>  port_out_forceSensors;
         OutputPort<AnalogMsg>  port_out_positionSensors;
         OutputPort<AnalogMsg>  port_out_analogIns;
-        OutputPort<EncoderMsg> port_out_linevoltage;
         OutputPort<EncoderMsg> port_out_timeStamp;
         InputPort<DigitalMsg>  port_in_digitalOuts;
         InputPort<AnalogMsg>   port_in_pwmDutyMotors;
@@ -237,7 +172,6 @@ namespace soem_beckhoff_drivers {
         InputPort<bool>        port_in_enable;
 
         uint16 print_counter;
-        float analogconverter;  // converter from 3.3V to 12bits
     };
 }
 #endif
